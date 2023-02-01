5a4e0684a65a7bcb61757ea712ebcef6c36f1894
musmanali123 <usman232429@gmail.com>
2023-02-01 20:16:45 +0500

    -make login

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:country_list_pick/country_list_pick.dart';
 import 'package:flutter/material.dart';
 
@@ -11,53 +13,142 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter P_P',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter_Practice_Project'),
+      debugShowCheckedModeBanner: true,
+      home: Scaffold(
+          backgroundColor: Colors.white,
+          body: SafeArea(
+              child: Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image(
+                      height: 100,
+                      width: 100,
+                      image: AssetImage('assets/download.jpg')),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'mainTAINCE',
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      Text('mainTAINCE',
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.red,
+                              fontWeight: FontWeight.w500))
+                    ],
+                  )
+                ],
+              ),
+              Center(
+                child: Text(
+                  "Login",
+                  style: TextStyle(
+                      color: Colors.orangeAccent,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Center(
+                child: Text(
+                  "Lorem ipsum Lorem ipsum Lorem ipsum \nLorem ipsum Lorem ipsum Lorem ipsum ",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w100),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 40, left: 20.1, right: 20.1),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    fillColor: Color(0xffF8F9FA),
+                    filled: true,
+                    prefixIcon: Icon(
+                      Icons.mail,
+                      color: Color(0xff323F4B),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Color(0xffE4E7EB)),
+                        borderRadius: BorderRadius.circular(10)),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xffE4E7EB)),
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20.1, right: 20.1, top: 40),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    fillColor: Color(0xffF8F9FA),
+                    filled: true,
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: Color(0xff323F4B),
+                    ),
+                    suffixIcon: Icon(Icons.visibility_off_outlined),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Color(0xffE4E7EB)),
+                        borderRadius: BorderRadius.circular(10)),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xffE4E7EB)),
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                ),
+              ),
+              // ignore: prefer_const_constructors
+              SizedBox(
+                height: 40,
+              ),
+              Container(
+                  width: 300,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(10.5)),
+                  child: Center(
+                    child: Text(
+                      "Login",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  )),
+              SizedBox(
+                height: 5,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Don"t have an account?'),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      color: Colors.orange,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ))),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: CountryListPick(
-          appBar: AppBar(
-            backgroundColor: Colors.amber,
-            title: const Text('Pick your country'),
-          ),
-          theme: CountryTheme(
-            isShowFlag: true,
-            isShowTitle: true,
-            isShowCode: true,
-            isDownIcon: true,
-            showEnglishName: true,
-            labelColor: Colors.blueAccent,
-          ),
-          initialSelection: '+44',
-          // or
-          // initialSelection: 'US'
-          // onChanged: (CountryCode code) {
-          //   // print(code.name);
-          //   // print(code.code);
-          //   // print(code.dialCode);
-          //   // print(code.flagUri);
-          // }
-        ));
   }
 }
