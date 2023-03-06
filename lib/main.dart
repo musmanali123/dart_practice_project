6562d7acefe54e70bcd6566b7e193c447517c2f7
musmanali123 <usman232429@gmail.com>
2023-03-06 23:52:21 +0500

    clr Picker

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,106 @@
-import 'dart:developer';
-
-import 'package:country_list_pick/country_list_pick.dart';
 import 'package:flutter/material.dart';
-import 'package:practice/view/searchbar.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MaterialApp(home: MyApp()));
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool lightTheme = true;
+  Color currentColor = Colors.amber;
+  List<Color> currentColors = [Colors.yellow, Colors.green];
+  List<Color> colorHistory = [];
+
+  void changeColor(Color color) => setState(() => currentColor = color);
+  void changeColors(List<Color> colors) =>
+      setState(() => currentColors = colors);
+
+  Color mycolor = Colors.lightBlue;
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: true, home: const SearchBarScreen());
+    return AnimatedTheme(
+      data: lightTheme ? ThemeData.light() : ThemeData.dark(),
+      child: Builder(builder: (context) {
+        return DefaultTabController(
+          length: 3,
+          child: Scaffold(
+            floatingActionButton: FloatingActionButton.extended(
+              onPressed: () => setState(() => lightTheme = !lightTheme),
+              icon: Icon(lightTheme
+                  ? Icons.dark_mode_rounded
+                  : Icons.light_mode_rounded),
+              label: Text(lightTheme ? 'Night' : '  Day '),
+              backgroundColor: currentColor,
+              elevation: 15,
+            ),
+            appBar: AppBar(
+              title: const Text('Flutter Color Picker Example'),
+              backgroundColor: currentColor,
+              // ignore: prefer_const_constructors
+              bottom: TabBar(
+                tabs: const <Widget>[
+                  Tab(text: 'HSV/HSL/RGB'),
+                  Tab(text: 'Material'),
+                  Tab(text: 'Blocky'),
+                ],
+              ),
+            ),
+            // ignore: prefer_const_constructors
+            body: TabBarView(
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text("JEEELELE", style: TextStyle(color: mycolor)),
+                    ElevatedButton(
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text(
+                                  'Pick a color!',
+                                ),
+                                content: SingleChildScrollView(
+                                  child: ColorPicker(
+                                    pickerColor: mycolor, //default color
+                                    onColorChanged: (Color color) {
+                                      //on color picked
+                                      setState(() {
+                                        mycolor = color;
+                                      });
+                                    },
+                                  ),
+                                ),
+                                actions: <Widget>[
+                                  ElevatedButton(
+                                    child: const Text('DONE'),
+                                    onPressed: () {
+                                      Navigator.of(context)
+                                          .pop(); //dismiss the color picker
+                                    },
+                                  ),
+                                ],
+                              );
+                            });
+                      },
+                      child: Text("Default Color Picker"),
+                    ),
+                  ],
+                ),
+                Text("JEEELELE"),
+                Text("JEEELELE")
+              ],
+            ),
+          ),
+        );
+      }),
+    );
   }
 }
--- lib/view/searchbar.dart
+++ lib/view/searchbar.dart
@@ -1,33 +1,62 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:fluttercontactpicker/fluttercontactpicker.dart';
 
-class SearchBarScreen extends StatefulWidget {
-  const SearchBarScreen({Key? key}) : super(key: key);
+void main() => runApp(const MaterialApp(home: MyApp()));
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<SearchBarScreen> createState() => _SearchBarScreenState();
+  State<StatefulWidget> createState() => _MyAppState();
 }
 
-class _SearchBarScreenState extends State<SearchBarScreen> {
+class _MyAppState extends State<MyApp> {
+  bool lightTheme = true;
+  Color currentColor = Colors.amber;
+  List<Color> currentColors = [Colors.yellow, Colors.green];
+  List<Color> colorHistory = [];
+
+  void changeColor(Color color) => setState(() => currentColor = color);
+  void changeColors(List<Color> colors) =>
+      setState(() => currentColors = colors);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('User List'),
-        ),
-        body: Center(
-          child: GestureDetector(
-            onTap: () async {
-              final FullContact contact =
-                  (await FlutterContactPicker.pickFullContact());
-
-              log("message${contact}");
-            },
-            child: Text("PRESS"),
+    return AnimatedTheme(
+      data: lightTheme ? ThemeData.light() : ThemeData.dark(),
+      child: Builder(builder: (context) {
+        return DefaultTabController(
+          length: 3,
+          child: Scaffold(
+            floatingActionButton: FloatingActionButton.extended(
+              onPressed: () => setState(() => lightTheme = !lightTheme),
+              icon: Icon(lightTheme
+                  ? Icons.dark_mode_rounded
+                  : Icons.light_mode_rounded),
+              label: Text(lightTheme ? 'Night' : '  Day '),
+              backgroundColor: currentColor,
+              elevation: 15,
+            ),
+            appBar: AppBar(
+              title: const Text('Flutter Color Picker Example'),
+              backgroundColor: currentColor,
+              bottom: TabBar(
+                tabs: const <Widget>[
+                  Tab(text: 'HSV/HSL/RGB'),
+                  Tab(text: 'Material'),
+                  Tab(text: 'Blocky'),
+                ],
+              ),
+            ),
+            body: TabBarView(
+              children: <Widget>[
+                Text("JEEELELE"),
+                Text("JEEELELE"),
+                Text("JEEELELE")
+              ],
+            ),
           ),
-        ));
+        );
+      }),
+    );
   }
 }
